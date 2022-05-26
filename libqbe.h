e1f7ae16981e9e7d5316f1f9a9d9577188579fc8
Kiëd Llaentenn <kiedtl@tilde.team>
2022-05-26 19:33:37 +0000

    fix: add __mulu32 to libqbe.h

--- libqbe.h
+++ libqbe.h
@@ -55,4 +55,6 @@ u32 __divu32_magic(u32 dvd, u32 dvs, u32 magic, bool a, u8 shift);
 u32 __divmodu32(u32 dvd, u32 dvs, u32 *rem_dest);
 u32 __modu32(u32 dvd, u32 dvs);
 
+u32 __mulu32(u32 a, u32 b);
+
 #endif
